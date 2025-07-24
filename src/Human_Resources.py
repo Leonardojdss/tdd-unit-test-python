ae114708be611bff31f2e21d48aac749e47a60d4
leonardojdss <leonardojpsedasilva@gmail.com>
2025-07-24 01:34:29 -0300

    em construção do treinamento

--- src/Human_Resources.py
+++ src/Human_Resources.py
@@ -19,12 +19,29 @@ class EmployeeOfBank:
         birth_year = birth_date_parts[-1]
         current_year = date.today().year
         return current_year - int(birth_year)
+    
+    @property
+    def surname(self):
+        name_complete = self.name.strip()
+        name_parts = name_complete.split(" ")
+        return " ".join(name_parts[1:])
 
     def calculate_bonus(self):
         value = self._salary * 0.1
         if value > 1000:
             value = 0
         return value
+    
+    @property
+    def decrease_salary(self):
+        salary_actual = self._salary
+        name_director = self._name
+        if salary_actual >= 100000 and name_director == "Leonardo José da Silva":    
+            salary_after = salary_actual * 0.9
+            print(salary_after)
+
+        return salary_after
 
     def __str__(self):
         return f'Employee({self._name}, {self._birth_date}, {self._salary})'
+
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -6,13 +6,40 @@ from src.Human_Resources import EmployeeOfBank
 class TestClass:
     def test_then_age_receive_13_03_2000_he_must_22(self):
         #given 
+        name="Funcionario_teste"
         birth_date = "13/03/2000"
         age = 25
 
-        employee = EmployeeOfBank("Funcionario_teste", birth_date, 2000)
-
         #When
+        employee = EmployeeOfBank(name, birth_date, 2000)
         result = employee.age()
 
         #Then
         assert result == age
+
+    def test_then_surname_receive_leonardo_jose_da_silva_he_must_jose_da_silva(self):
+        #given
+        birth_date = "13/03/2000"
+        surname = "jose da silva"
+        name="Leonardo jose da silva"
+
+        #When
+        employee = EmployeeOfBank(name, birth_date, 2000)
+        result = employee.surname
+
+        #then
+        assert result == surname
+
+    def test_then_decrease_salary_receive_100000_he_must_90000(self):
+        #given
+        name="Leonardo José da Silva"
+        salary_actual = 100000
+        birth_date = "13/03/2000"
+        salary_after = 90000
+
+        #When
+        employee = EmployeeOfBank(name, birth_date, salary_actual)
+        result = employee.decrease_salary
+
+        #then
+        assert result == salary_after
